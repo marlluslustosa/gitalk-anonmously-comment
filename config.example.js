dfebf288801585445e89ca060c205dc2a9a5706a
Marllus Lustosa <2443853-mlustosa@users.noreply.gitlab.com>
2021-06-15 12:57:29 -0300

    config atlerado

--- config.example.js
+++ config.example.js
@@ -1,8 +1,8 @@
 module.exports = {
-  appId: 'your github appid',
-  appSecret: 'secret',
-  origin: 'http://example.com', //允许跨域的域名
-  accessToken: 'person access token',
+  appId: '3b56f6c043a50d32dd82',
+  appSecret: '0b0ea67ff9b13f768064aacf117643b45759b1ba',
+  origin: 'https://marllus.com', //允许跨域的域名
+  accessToken: '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',
   state: 'random string',
-  redirectURI: 'https://example.com/api/access_token' //授权成功跳转地址
+  redirectURI: 'https://marllus.com/api/access_token' //授权成功跳转地址
 }
