e06b875b9bfa7097c820fd9138cd3cff27db86ec
Marllus Lustosa <2443853-mlustosa@users.noreply.gitlab.com>
2021-06-16 12:41:53 -0300

    mudando api authorize

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
   appId: '3b56f6c043a50d32dd82',
   appSecret: '0b0ea67ff9b13f768064aacf117643b45759b1ba',
-  origin: '*', //允许跨域的域名
+  origin: 'https://48bbf2b1.myblog-bkz.pages.dev', //允许跨域的域名
   accessToken: `process.env.GH_PAT`,
   state: 'random string',
 //redirectURI: 'https://48bbf2b1.myblog-bkz.pages.dev/api/access_token' //授权成功跳转地址
